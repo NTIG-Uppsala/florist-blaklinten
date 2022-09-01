3a3fa3500fd74ba31e561c7759997e1d16019698
Gustavsson123 <vilhelm.gustavsson@elev.ga.ntig.se>
2022-09-01 13:39:33 +0200

    vi har fixat kommentarer

--- tests/seleniumTests.py
+++ tests/seleniumTests.py
@@ -12,29 +12,29 @@ class CheckSiteAvailability(unittest.TestCase):
     def setUp(self):
         driver_path = ChromeDriverManager(chrome_type=ChromeType.CHROMIUM).install() # Initializes the driver
 
+        # Run the browser with no GUI
         chrome_options = Options()
         chrome_options.add_argument("--headless") 
 
-        self.browser = webdriver.Chrome(driver_path, options=chrome_options) 
-        self.addCleanup(self.browser.quit) 
+        self.browser = webdriver.Chrome(driver_path, options=chrome_options) # Initializes the browser instance with the driver
+        self.addCleanup(self.browser.quit) # Closes browser instance when the tests are done
 
+    # Check if "Florist Blåklinten" is in the <title> of the page
     def test_page_title(self):
         self.browser.get(self.website_url)
         self.assertIn("Florist Blåklinten", self.browser.title) 
 
-    def test_page_name(self):
-        self.browser.get(self.website_url)
-        self.browser.find_element(By.TAG_NAME, "header")
-
-
+    # Tests if the class "fa-facebook" is on the page
     def test_check_facebook(self):
         self.browser.get(self.website_url)
         self.browser.find_element(By.CLASS_NAME, "fa-facebook")
 
+    # Tests if the class "fa-twitter" is on the page
     def test_check_twitter(self):
         self.browser.get(self.website_url)
         self.browser.find_element(By.CLASS_NAME, "fa-twitter")
 
+    # Tests if the class "fa-instagram" is on the page
     def test_check_instagram(self):
         self.browser.get(self.website_url)
         self.browser.find_element(By.CLASS_NAME, "fa-instagram")
