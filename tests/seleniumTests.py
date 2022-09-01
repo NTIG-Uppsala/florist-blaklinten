48ab4cfc08ba75d1712d7c68493dd1de3309f181
Gustavsson123 <vilhelm.gustavsson@elev.ga.ntig.se>
2022-09-01 10:36:11 +0200

    vi har fixade tester och flyttade lite mappar

---
+++ tests/seleniumTests.py
@@ -0,0 +1,43 @@
+import unittest
+from pathlib import Path
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from webdriver_manager.chrome import ChromeDriverManager
+from webdriver_manager.core.utils import ChromeType
+from selenium.webdriver.common.by import By
+
+class CheckSiteAvailability(unittest.TestCase):
+    website_url = "http://127.0.0.1:5500/src/index.html" # Standard URL 
+
+    def setUp(self):
+        driver_path = ChromeDriverManager(chrome_type=ChromeType.CHROMIUM).install() # Initializes the driver
+
+        chrome_options = Options()
+        chrome_options.add_argument("--headless") 
+
+        self.browser = webdriver.Chrome(driver_path, options=chrome_options) 
+        self.addCleanup(self.browser.quit) 
+
+    def test_page_title(self):
+        self.browser.get(self.website_url)
+        self.assertIn("Florist Blåklinten", self.browser.title) 
+
+    def test_page_name(self):
+        self.browser.get(self.website_url)
+        self.browser.find_element(By.TAG_NAME, "header")
+
+
+    def test_check_facebook(self):
+        self.browser.get(self.website_url)
+        self.browser.find_element(By.CLASS_NAME, "fa-facebook")
+
+    def test_check_twitter(self):
+        self.browser.get(self.website_url)
+        self.browser.find_element(By.CLASS_NAME, "fa-twitter")
+
+    def test_check_instagram(self):
+        self.browser.get(self.website_url)
+        self.browser.find_element(By.CLASS_NAME, "fa-instagram")
+
+if __name__ == "__main__":
+    unittest.main()
