009344ecf5cd60e128604b765dea30f8fa7df22a
Kastrullen <roder.gumuscu@elev.ga.ntig.se>
2022-09-28 14:37:10 +0200

    Changed translation
    Co-Authored-By: Kasper-coder <111971931+Kasper-coder@users.noreply.github.com>
    Co-Authored-By: Linus J <30862662+linus-jansson@users.noreply.github.com>

--- tests/seleniumTests.py
+++ tests/seleniumTests.py
@@ -127,7 +127,7 @@ class CheckWebsite(unittest.TestCase):
         ]
 
         closedDays = [
-            "Вихідні дні",
+            "Неробочі дні",
             "Новий рік",
             "1 січня",
             "Тринадцятий день Різдва",
